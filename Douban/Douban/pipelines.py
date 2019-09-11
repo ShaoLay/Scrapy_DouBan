4ee0010ff515db5071f19e60dec9c4048d7f347c
ShaoLeei <javs_shao@163.com>
2019-09-11 13:46:18 +0800

    保存单页数据

--- Douban/Douban/pipelines.py
+++ Douban/Douban/pipelines.py
@@ -4,6 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
 class DoubanPipeline(object):
@@ -11,6 +12,8 @@ class DoubanPipeline(object):
         self.file = open('douban.json', 'w')
 
     def process_item(self, item, spider):
+        content = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(content)
         return item
 
     def close_spider(self, spider):
