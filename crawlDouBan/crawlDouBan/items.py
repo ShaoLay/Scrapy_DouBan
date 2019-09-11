417864de71339b8f2c4a43ce50c90883329eb863
ShaoLeei <javs_shao@163.com>
2019-09-11 15:33:39 +0800

    crawl_爬取单页数据_待完善_正则表达式

--- crawlDouBan/crawlDouBan/items.py
+++ crawlDouBan/crawlDouBan/items.py
@@ -11,4 +11,5 @@ import scrapy
 class CrawldoubanItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    stars = scrapy.Field()
+    # comment = scrapy.Field()
--- crawlDouBan/crawlDouBan/pipelines.py
+++ crawlDouBan/crawlDouBan/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
 class CrawldoubanPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('stars.json', 'w')
+
     def process_item(self, item, spider):
+        content = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(content)
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
